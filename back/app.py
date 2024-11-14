078f06f598479d2ce2fdbcd4e831e8ce78e1a573
DANIELXXOMG2 <danielsebastiank6@gmail.com>
2024-11-14 17:42:12 -0500

    Se ajusta titulo principal en index y en proceso de ajustes generales

--- back/app.py
+++ back/app.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import mysql.connector
 from mysql.connector import Error
 
-
-
 app = Flask(__name__, template_folder="../html/", static_folder="../front/")
-       
-
+app.secret_key = '40334277'
 def inicializar_bd():
     try:
         conexion = mysql.connector.connect(
@@ -23,6 +20,7 @@ def inicializar_bd():
             
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS Usuarios (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
                     nombre VARCHAR(255) NOT NULL,
                     email VARCHAR(255) NOT NULL,
                     password_ VARCHAR(255) NOT NULL
@@ -35,6 +33,30 @@ def inicializar_bd():
         if conexion.is_connected():
             cursor.close()
             conexion.close()
+
+
+def verificar_usuario(email, password_):
+    try:
+        conexion = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='40334277',
+            database='insectos_polinizadores'
+        )
+        if conexion.is_connected():
+            cursor = conexion.cursor()
+            consulta = "SELECT * FROM usuarios WHERE email = %s AND password_ = %s"
+            cursor.execute(consulta, (email, password_))
+            usuario = cursor.fetchone()
+            return usuario is not None
+    except Error as e:
+        print("Error al verificar el usuario:", e)
+    finally:
+        if conexion.is_connected():
+            cursor.close()
+            conexion.close()
+    return False
+
 
 def registrar_usuario_en_bd(nombre, email, password_):
     try:
@@ -61,32 +83,29 @@ def registrar_usuario_en_bd(nombre, email, password_):
             conexion.close()
 
 @app.route('/')
+def login2():
+    return render_template('login.html')
+
+
+@app.route('/register')
 def register():
     return render_template('register.html')
 
 @app.route('/index')
 def index():
-    return render_template('index.html')  
-
-@app.route('/login')
-def login():
-    return render_template('login.html')  
-
-@app.route('/categorias')
-def categorias():
-    return render_template('categorias.html')  
+    return render_template('index.html')
 
 @app.route('/contacto')
 def contacto():
-    return render_template('contacto.html')  
+    return render_template('contacto.html')
 
 @app.route('/reviews')
 def reviews():
-    return render_template('reviews.html')  
+    return render_template('reviews.html')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html')  
+    return render_template('sobre.html')
 
 
 @app.route('/registrar_usuario', methods=['POST'])
@@ -97,6 +116,44 @@ def registrar_usuario():
     registrar_usuario_en_bd(nombre, email, password_)
     return redirect(url_for('register'))
 
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    email = request.form['email'] 
+    password_ = request.form['password_'] 
+    
+    if verificar_usuario(email, password_):
+        
+        conexion = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='40334277',
+            database='insectos_polinizadores'
+        )
+        cursor = conexion.cursor()
+        cursor.execute("SELECT nombre FROM usuarios WHERE email = %s", (email,))
+        usuario = cursor.fetchone()
+        nombre = usuario[0] if usuario else None
+        
+        session['email'] = email  
+        session['nombre'] = nombre
+        
+        flash('Inicio de sesión exitoso')
+        return redirect(url_for('index'))  
+    else:
+        flash('Correo o contraseña incorrectos')
+        return redirect(url_for('login'))
+
+
+
 if __name__ == "__main__":
     inicializar_bd()  
     app.run(debug=True)
+
+
+# @app.route('/logout')
+# def logout():
+#     session.pop('email', None)
+#     session.pop('nombre_usuario', None)
+#     flash('Has cerrado sesión exitosamente')
+#     return redirect(url_for('index'))
